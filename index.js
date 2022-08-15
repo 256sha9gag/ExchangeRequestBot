3d28db9c567e610a88aceb3f6b43da5cba060487
gag <256sha9gag@gmail.com>
2022-08-16 00:12:41 +0300

    feat: Creat tg-api

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import TelegramBot from "node-telegram-bot-api";
-import token from "./token";
+import token from "./token.js";
 
+const bot = new TelegramBot(token, { polling: true });
 
+bot.on('message', msg => {
+  console.log(msg);
+});
